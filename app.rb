88c0b81865d1e50e9da1268d9a0aaa2b8f018317
Tom Dalling <tom@tomdalling.com>
2019-06-11 20:18:41 +1000

    hooks

--- app.rb
+++ app.rb
@@ -3,3 +3,16 @@ require "sinatra"
 get "/" do
   "Hello world!"
 end
+
+post "/neato/order_dispatched" do
+  request.body.rewind
+  Nokogiri::HTML(request.body.read
+  request_body = JSON.parse(request.body.read)
+  [200, {}, "OK"]
+end
+
+post "/groupon/order_created" do
+  request.body.rewind
+  request_body = JSON.parse(request.body.read)
+  [200, {}, "OK"]
+end
