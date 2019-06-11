1720192a4c040dcb1ea19fac17d50b9eaeeff5ef
Tom Dalling <tom@tomdalling.com>
2019-06-11 19:53:44 +1000

    Stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/groupon/order_created', to: 'groupon#order_created'
+  post '/groupon/order_created', to: 'groupon#order_created'
+  get '/neato/dispatched', to: 'neato#dispatched'
+  post '/neato/dispatched', to: 'neato#dispatched'
 end
